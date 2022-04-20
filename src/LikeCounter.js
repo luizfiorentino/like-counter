8c16bec8f9e21b3d3f3943e291361b493ea9afb8
luizfiorentino <lgfiorentino@gmail.com>
2022-04-20 12:04:53 +0200

    click-unclick button added

--- src/LikeCounter.js
+++ src/LikeCounter.js
@@ -3,7 +3,8 @@ import { useState } from "react";
 export default function LikeCounter() {
   const initial_Likes = 0;
   const [numLikes, setNumLikes] = useState(initial_Likes);
-  console.log("What are these?", numLikes, setNumLikes);
+  //console.log("What are these?", numLikes, setNumLikes);
+  const [activeOrInactive, setActiveOrInactive] = useState(true);
 
   const incrementLikes = () => {
     console.log("Yes, clicked! Current number of likes", numLikes);
@@ -14,12 +15,19 @@ export default function LikeCounter() {
     setNumLikes(initial_Likes);
   };
 
+  const clickUnclick = () => {
+    setActiveOrInactive(!activeOrInactive);
+  };
+
   return (
     <div>
       <p>
         This post has <b>{numLikes}</b> Likes!{" "}
-        <button onClick={incrementLikes}>Like</button>
-        <button onClick={resetLikes}>Reset likes</button>
+        <button onClick={incrementLikes}>Like</button>{" "}
+        <button onClick={resetLikes}>Reset likes</button>{" "}
+        <button onClick={clickUnclick}>
+          {activeOrInactive ? " CLICK ME!" : "UNCLICK ME!"}
+        </button>
       </p>
     </div>
   );
