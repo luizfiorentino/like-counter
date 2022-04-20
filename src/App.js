fc5cbc624d8c21264b1f58ac5aba6c03975a8f64
luizfiorentino <lgfiorentino@gmail.com>
2022-04-20 12:21:19 +0200

    mapping over an array of strings and renderring it in html

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import LikeCounter from "./LikeCounter";
 import Title from "./Title";
+import AwesomeAnimals from "./AwesomeAnimals";
 
 function App() {
   return (
     <div className="App">
       <Title content="Welcome to the like counter!" />
       <LikeCounter />
+      <AwesomeAnimals />
     </div>
   );
 }
---
+++ src/AwesomeAnimals.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function AwesomeAnimals() {
+  return (
+    <ul>
+      {["Chicken", "Sloth", "Porcupine", "Killer whale", "Velociraptor"].map(
+        (animal, index) => {
+          return (
+            <li key={index}>
+              {" "}
+              Awesomeness level {index + 1}: {animal}
+            </li>
+          );
+        }
+      )}
+    </ul>
+  );
+}
